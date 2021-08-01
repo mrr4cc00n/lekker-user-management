867de9ba1d0610b8304c433b0fb9c0c5ff513526
r4cc00n <manuel@tetra.team>
2021-08-01 21:14:50 +0200

    added change status to the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'users/change_status'
   resource :signup, only: %i[create]
   resources :authentications, only: %i[create]
   resources :users, only: %i[index]
