bf853a8741d681846b02676f243bd1356ebdbfe0
r4cc00n <manuel@tetra.team>
2021-08-01 21:29:22 +0200

    added rswag to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   post 'users/change_status'
   resource :signup, only: %i[create]
   resources :authentications, only: %i[create]
